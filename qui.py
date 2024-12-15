5dc05e20f8c1e2578473fe59b9200f50785f73b4
divya-ct <guddan.282005@gmail.com>
2024-12-15 22:35:54 +0530

    Create qui.py

---
+++ qui.py
@@ -0,0 +1,31 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+def print_top5(arr):
+    arr.reverse()
+    if len(arr) < 5:
+        for i in range(0, len(arr)):
+            print(arr[i])
+    else:
+        for i in range(0, 5):
+            print(arr[i])
+
+
+percentages = []
+n = int(input("\nEnter Number of students in first year: "))
+for i in range(n):
+    percentage = float(input(f"\nEnter Percentage of student {i+1}: "))
+    percentages.append(percentage)
+
+print("\nPercentages in ascending order using Quick Sort: ")
+sorted_percentages = quick_sort(percentages)
+print(sorted_percentages)
+print("\nTop 5 Scores are: ")
+print_top5(sorted_percentages)
